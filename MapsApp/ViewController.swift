58cebb0031dd2861bca208bd85a8421213cf36e6
Wim Sonevel <wim.sonevel@yahoo.co.id>
2023-05-21 21:34:20 +0700

    Initial Commit

---
+++ MapsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapsApp
+//
+//  Created by Kwikku Nusantara on 21/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
